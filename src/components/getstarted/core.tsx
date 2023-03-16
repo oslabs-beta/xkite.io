57da047b4bade7719fb2b04944d605efd11e8ecd
zahara aviv <zahara.aviv@gmail.com>
2023-03-16 12:30:33 -0400

    edited the import of the core library examples

--- src/components/getstarted/core.tsx
+++ src/components/getstarted/core.tsx
@@ -1,26 +1,21 @@
 import TabPanel from '@mui/lab/TabPanel';
-import Image from 'next/image'
-import {
-  Box,
-  Typography,
-  styled
-} from '@mui/material';
+import Image from 'next/image';
+import { Box, Typography, styled } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
-import xkiteCore from 'public/xkite-core.gif'
+import xkiteCore from 'public/xkite-core.gif';
 import { CopyBlock, atomOneDark } from 'react-code-blocks';
-
 
 const useStyles = makeStyles({
   root: {
     marginTop: '1.5em',
-    marginBottom: '1.5em'
+    marginBottom: '1.5em',
   },
   leftText: {
-    textAlign: "left"
+    textAlign: 'left',
   },
   rightText: {
-    textAlign: "right"
-  }
+    textAlign: 'right',
+  },
 });
 
 const Step = styled(Typography)(
@@ -56,95 +51,131 @@ export default function core() {
   const classes = useStyles();
   return (
     <>
-    <Panel value='core' className={classes.leftText} >
-    <Box className={classes.root}></Box>
-    <Box className={classes.root}>
-      <Description >The guide below uses the following repository: </Description>
-      </Box>
-      <Code className={classes.root}>
-        <CopyBlock
-          text={`https://github.com/oslabs-beta/xkite-core`}
-          language='javascript'
-          showLineNumbers={true}
-          wrapLines
-          codeBlock
-          theme={atomOneDark}
-        />
-      </Code>
-      <Box className={classes.root}>
-      <Step >System Requirements: </Step>
-      </Box>
-      <Box className={classes.root}>
-      <Description >- Latest stable versions of <a href="https://docs.npmjs.com/downloading-and-installing-node-js-and-npm">Node.js and NPM</a> installed</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Description >- Latest stable versions of <a href="https://docs.docker.com/compose/install/">docker-compose</a> installed</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Description >- <strong>Note: </strong>You will need Docker daemon running before deploying an xkite configuration.</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Step >Overview: </Step>
-      </Box>
-    <Box className={classes.root}>
-      <Description >The xkite-core library is, as the name suggests, the core library for xkite. It provides the underpinning functionality 
-                    for configuring a YAML file, managing docker containers (configure, run, pause, and shutdown), interfacing with remote 
-                    xkite servers, and providing configuration settings for users to easily connect to their Kafka instances for development purposes.</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Description >The xkite-core library is available as an npm package to enable developers to integrate the tool into their existing codebase 
-                    or kickstart their application using Kafka. Two practical use cases for using xkite-core are already shown above with the xkite 
-                    web GUI and the xkite-cli applications. </Description>
-      </Box>
-      <Box className={classes.root}>
-      <Step >1. Install xkite-core into your existing codebase using NPM</Step>
-      </Box>
-      <Box className={classes.root}>
-      <Description >a. Run the following command in your terminal at the root of your project directory:</Description>
-      </Box>
-      <Code className={classes.root}>
-        <CopyBlock
-          text={`npm install xkite-core`}
-          language='javascript'
-          showLineNumbers={true}
-          wrapLines
-          codeBlock
-          theme={atomOneDark}
-        />
-      </Code>
-      <Image src={xkiteCore} alt="xkite-cli" />
-      <Box className={classes.root}>
-      <Description >After installation is completed, you&apos;re ready to begin using xkite-core across your application architecture.</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Step >Using xkite-core</Step>
-      </Box>
-      <Box className={classes.root}>
-      <Description >Importing types:</Description>
-      </Box>
-      <Code className={classes.root}>
-        <CopyBlock
-          text={`import type { KiteConfig, KiteSetup, KiteClass } from 'xkite-core';`}
-          language='javascript'
-          showLineNumbers={true}
-          wrapLines
-          codeBlock
-          theme={atomOneDark}
-        />
-      </Code>
-      <Box className={classes.root}>
-      <Description >(Full list of interfaces and types can be found on <a href="https://www.npmjs.com/package/xkite-core">NPM</a>)</Description>
-      </Box>
-      <Box className={classes.root}>
-      <Description >Using library methods example (using next.js):</Description>
-      </Box>
-      <Code className={classes.root}>
-        <CopyBlock
-          text={`import type { NextApiRequest, NextApiResponse } from 'next/types';
-          let { Kite } = require('xkite-core');
-          if (Kite === undefined) {
-            Kite = require('xkite-core').default;
-          }
+      <Panel value='core' className={classes.leftText}>
+        <Box className={classes.root}></Box>
+        <Box className={classes.root}>
+          <Description>
+            The guide below uses the following repository:{' '}
+          </Description>
+        </Box>
+        <Code className={classes.root}>
+          <CopyBlock
+            text={`https://github.com/oslabs-beta/xkite-core`}
+            language='javascript'
+            showLineNumbers={true}
+            wrapLines
+            codeBlock
+            theme={atomOneDark}
+          />
+        </Code>
+        <Box className={classes.root}>
+          <Step>System Requirements: </Step>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            - Latest stable versions of{' '}
+            <a href='https://docs.npmjs.com/downloading-and-installing-node-js-and-npm'>
+              Node.js and NPM
+            </a>{' '}
+            installed
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            - Latest stable versions of{' '}
+            <a href='https://docs.docker.com/compose/install/'>
+              docker-compose
+            </a>{' '}
+            installed
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            - <strong>Note: </strong>You will need Docker daemon running before
+            deploying an xkite configuration.
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Step>Overview: </Step>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            The xkite-core library is, as the name suggests, the core library
+            for xkite. It provides the underpinning functionality for
+            configuring a YAML file, managing docker containers (configure, run,
+            pause, and shutdown), interfacing with remote xkite servers, and
+            providing configuration settings for users to easily connect to
+            their Kafka instances for development purposes.
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            The xkite-core library is available as an npm package to enable
+            developers to integrate the tool into their existing codebase or
+            kickstart their application using Kafka. Two practical use cases for
+            using xkite-core are already shown above with the xkite web GUI and
+            the xkite-cli applications.{' '}
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Step>
+            1. Install xkite-core into your existing codebase using NPM
+          </Step>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            a. Run the following command in your terminal at the root of your
+            project directory:
+          </Description>
+        </Box>
+        <Code className={classes.root}>
+          <CopyBlock
+            text={`npm install xkite-core`}
+            language='javascript'
+            showLineNumbers={true}
+            wrapLines
+            codeBlock
+            theme={atomOneDark}
+          />
+        </Code>
+        <Image src={xkiteCore} alt='xkite-cli' />
+        <Box className={classes.root}>
+          <Description>
+            After installation is completed, you&apos;re ready to begin using
+            xkite-core across your application architecture.
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Step>Using xkite-core</Step>
+        </Box>
+        <Box className={classes.root}>
+          <Description>Importing types:</Description>
+        </Box>
+        <Code className={classes.root}>
+          <CopyBlock
+            text={`import type { KiteConfig, KiteSetup, KiteClass } from 'xkite-core';`}
+            language='javascript'
+            showLineNumbers={true}
+            wrapLines
+            codeBlock
+            theme={atomOneDark}
+          />
+        </Code>
+        <Box className={classes.root}>
+          <Description>
+            (Full list of interfaces and types can be found on{' '}
+            <a href='https://www.npmjs.com/package/xkite-core'>NPM</a>)
+          </Description>
+        </Box>
+        <Box className={classes.root}>
+          <Description>
+            Using library methods example (using next.js):
+          </Description>
+        </Box>
+        <Code className={classes.root}>
+          <CopyBlock
+            text={`import type { NextApiRequest, NextApiResponse } from 'next/types';
+          const { Kite } = require('xkite-core');
           
           export default function handler(req: NextApiRequest, res: NextApiResponse) {
             if (req.method === 'GET') {
@@ -154,23 +185,20 @@ export default function core() {
               res.status(405).send('Method Not Allowed');
             }
           }`}
-          language='javascript'
-          showLineNumbers={true}
-          wrapLines
-          codeBlock
-          theme={atomOneDark}
-        />
-      </Code>
-      <Box className={classes.root}>
-      <Description >Getting Kite setup example: </Description>
-      </Box>
-      <Code className={classes.root}>
-        <CopyBlock
-          text={`import type { NextApiRequest, NextApiResponse } from 'next/types';
-          let { Kite } = require('xkite-core');
-          if (Kite === undefined) {
-            Kite = require('xkite-core').default;
-          }
+            language='javascript'
+            showLineNumbers={true}
+            wrapLines
+            codeBlock
+            theme={atomOneDark}
+          />
+        </Code>
+        <Box className={classes.root}>
+          <Description>Getting Kite setup example: </Description>
+        </Box>
+        <Code className={classes.root}>
+          <CopyBlock
+            text={`import type { NextApiRequest, NextApiResponse } from 'next/types';
+          const { Kite } = require('xkite-core');
           import type { KiteSetup } from 'xkite-core';
           
           type Setup = KiteSetup;
@@ -192,14 +220,14 @@ export default function core() {
               res.status(405).send('Method Not Allowed');
             }
           }`}
-          language='javascript'
-          showLineNumbers={true}
-          wrapLines
-          codeBlock
-          theme={atomOneDark}
-        />
-      </Code>
-    </Panel>
+            language='javascript'
+            showLineNumbers={true}
+            wrapLines
+            codeBlock
+            theme={atomOneDark}
+          />
+        </Code>
+      </Panel>
     </>
   );
 }
